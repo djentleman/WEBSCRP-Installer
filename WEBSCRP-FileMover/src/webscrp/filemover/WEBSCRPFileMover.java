af08395acf1547ac33d012b7c2b120934e053833
Todd Perry <up612136@myport.ac.uk>
2013-03-28 21:00:10 +0000

    Setting Up Java

---
+++ WEBSCRP-FileMover/src/webscrp/filemover/WEBSCRPFileMover.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webscrp.filemover;
+
+/**
+ *
+ * @author U AMD
+ */
+public class WEBSCRPFileMover {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
