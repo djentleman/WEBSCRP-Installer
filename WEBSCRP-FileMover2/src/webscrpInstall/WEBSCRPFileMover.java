c5fc16e0cbf1622f264525ef8b4b24e0f72f20a2
Todd Perry <up612136@myport.ac.uk>
2013-04-09 22:05:08 +0100

    file structure change

---
+++ WEBSCRP-FileMover2/src/webscrpInstall/WEBSCRPFileMover.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package webscrpInstall;
+
+/**
+ *
+ * @author U AMD
+ */
+public class WEBSCRPFileMover {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GUI gui = new GUI(); // init gui
+        gui.main(null);
+    }
+}
